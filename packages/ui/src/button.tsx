ed662354174f05feadfb887bd504dccb88cc0e04
ellyraad <76833632+ellyraad@users.noreply.github.com>
2025-10-23 13:54:35 +0800

    cleanup and format files

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,16 @@
 "use client";
 
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+	children: ReactNode;
+	className?: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
+export const Button = ({ children, className }: ButtonProps) => {
+	return (
+		<button type="button" className={className}>
+			{children}
+		</button>
+	);
 };
